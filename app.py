c93048c159cf26641237d738b3979a2b14b28989
Tuğçe Şahin <112752351+tugcesahiint@users.noreply.github.com>
2025-04-02 23:55:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+# Flask uygulaması
+app = Flask(__name__, static_folder='static')
 
+# Ana sayfa olarak index.html'yi sun
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('static', 'index.html')
 
+# Uygulama sunucusu
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 10000))  # Render gibi servisler portu dışarıdan verir
     app.run(host='0.0.0.0', port=port)
